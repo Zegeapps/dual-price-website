976f92648bd052563629c6fc7d9cf15dfc5f736c
sreerag5375 <sreeragsree175@gmail.com>
2025-03-30 11:13:42 +0530

    clean next config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/dual-price-website',
-  assetPrefix: '/dual-price-website/',
   images: {
     unoptimized: true,
   }
