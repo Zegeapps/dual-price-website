415618127c877384b9e245367002d453f133594b
sreerag5375 <sreeragsree175@gmail.com>
2025-04-08 10:26:00 +0530

    change font to poppoins

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,12 @@
-import { Lato } from "next/font/google";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const lato = Lato({
-  weight: ['400', '700', '900'],
-  subsets: ["latin"],
-  variable: "--font-lato",
-});
-
-const inter = Inter({
+const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
   subsets: ["latin"],
-  variable: "--font-inter",
+  variable: "--font-poppins",
 });
+
 export const metadata = {
   title: "VAT: Dual Pricing PRO",
   description: "Shopify app for display VAT prices on store",
@@ -21,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${lato.variable} ${inter.variable} antialiased`}>
+       <body className={`${poppins.variable} antialiased`}>
       {children}
       </body>
     </html>
